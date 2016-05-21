01901894e8e39c3e75883cfe8000c6022d66c1d4
yon <Lighthawky@gmail.com>
2016-05-21 17:22:25 +0300

    invite

--- config.rb
+++ config.rb
@@ -53,10 +53,13 @@ set :js_dir, 'javascripts'
 
 set :images_dir, 'images'
 
+activate :directory_indexes
+
 # Build-specific configuration
 configure :build do
   # For example, change the Compass output style for deployment
   activate :minify_css
+
 
   # Minify Javascript on build
   activate :minify_javascript
